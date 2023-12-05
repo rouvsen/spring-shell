35252ca408eba38f4a78bab9ba4775bf09265deb
rouvsen <rouvsen@gmail.com>
2023-12-05 13:42:20 +0400

    Add Feign Client Configurations to project

---
+++ src/main/java/com/example/springshell/FeignClientConfig.java
@@ -0,0 +1,14 @@
+package com.example.springshell;
+
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class FeignClientConfig {
+//    @Bean
+//    RequestInterceptor requestInterceptor(@Value("${api.key}") String apikey) {
+//        return requestTemplate -> requestTemplate.header("authorization", apikey);
+//    }
+}
--- src/main/java/com/example/springshell/SpringShellApplication.java
+++ src/main/java/com/example/springshell/SpringShellApplication.java
@@ -2,9 +2,11 @@ package com.example.springshell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.shell.command.annotation.CommandScan;
 
 @SpringBootApplication
+@EnableFeignClients
 @CommandScan
 public class SpringShellApplication {
 
