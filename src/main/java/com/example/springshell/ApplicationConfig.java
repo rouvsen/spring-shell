43f236603411b5d9d6fbb7a3e7e8512d15e13948
rouvsen <rouvsen@gmail.com>
2023-12-08 05:00:39 +0400

    Add ShellReader for getting data from console

--- src/main/java/com/example/springshell/ApplicationConfig.java
+++ src/main/java/com/example/springshell/ApplicationConfig.java
@@ -1,12 +1,19 @@
 package com.example.springshell;
 
+import org.jline.reader.LineReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class ApplicationConfig {
     @Bean
     CLIExceptionHandler exceptionHandler() {
         return new CLIExceptionHandler();
+    }
+
+    @Bean
+    public ShellReader shellReader(@Lazy LineReader lineReader) {
+        return new ShellReader(lineReader);
     }
 }
